8eedff2bdcda689ae06b38101d5cda3c82c9e65f
puchura <btc3171@gmail.com>
2024-07-09 12:27:21 +0400

    test
    this is a test
    
    more test

--- core/utils.go
+++ core/utils.go
@@ -44,37 +44,6 @@ func (d Dice) RollWithAdvantage(advantage int) int {
 }
 
 
-func (d Dice) Roll(reroll int) int {
-	result := 0
-	rerollvalue := math.Abs(float64(reroll))
-	roll := 0
-	for i := 0; i < d.Amount; i++ {
-		roll = rand.Intn(d.Size) + 1
-		if reroll > 0 {
-			roll = rand.Intn(int(rerollvalue)-1) + 1
-		}
-		if reroll < 0 {
-			roll = rand.Intn(d.Size-int(rerollvalue)) + int(rerollvalue) + 1
-		}
-		result += roll
-	}
-	return result
-}
-
-
-func RollDice(dice string) (int, []int) {
-	d := strings.Split(dice, "d")
-	damount, _ := strconv.Atoi(d[0])
-	dsize, _ := strconv.Atoi(d[1])
-	rolls := make([]int, damount)
-	roll := 0
-	for i := 0; i < damount; i++ {
-		droll := rand.Intn(dsize) + 1
-		rolls[i] = droll
-		roll += droll
-	}
-	return roll, rolls
-}
 
 func Rolldwithadv(dice string, advantage int) (int, []int) {
 	adv := int(math.Abs(float64(advantage)))
