514618e8aa99be7cd38126831c7f8f03cce378a3
puchura <btc3171@gmail.com>
2024-07-10 20:06:36 +0400

    basic window with reylib

--- game.go
+++ game.go
@@ -7,22 +7,51 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const (
+	stitle  string = "Super Duper Mega Game"
+	sheight int32  = 800
+	swidth  int32  = 450
+)
+
+var (
+	running bool = true
+	bgcolor      = rl.NewColor(60, 158, 250, 255)
+)
+
+func drawScene() {}
+func input()     {}
+func update() {
+	running = !rl.WindowShouldClose()
+}
+func render() {
+	rl.BeginDrawing()
+
+	rl.ClearBackground(bgcolor)
+	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+	rl.EndDrawing()
+}
+
+func init() {
+	rl.InitWindow(sheight, swidth, stitle)
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+}
+func quit() {
+	rl.CloseWindow()
+}
+
 func main() {
 
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
-	defer rl.CloseWindow()
-
-	rl.SetTargetFPS(60)
-
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
-		rl.EndDrawing()
+	for running {
+		input()
+		update()
+		render()
 	}
 
+}
+
+func test() {
 	//dice := core.Dice{Amount: 1, Size: 6}
 
 	//dice, die := core.Rolldwithadv("2d10", 0)
@@ -38,5 +67,4 @@ func main() {
 		fmt.Println(dice.RollWithAdvantage(0, -4))
 	}
 	*/
-
 }
