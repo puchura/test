49a2b447d87d906ebd45cfbae614a4686c548289
Puchura <btc3171@gmail.com>
2024-07-19 17:25:03 +0400

    Added basic map render, 3d camera and camera controls

--- scenes/game.go
+++ scenes/game.go
@@ -1,6 +1,51 @@
 package scenes
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"wgame/core"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+var (
+	gmap      core.GameMap
+	gridSize  int = 10
+	cellSize  int = 50
+	camera    rl.Camera3D
+	panSpeed          = 5.0
+	panBorder         = 100
+	dragspeed float32 = 0.25
+)
+
+func drawGrid() {
+
+	// Calculate the total grid dimensions
+	gridWidth := float32(gridSize * cellSize)
+	gridHeight := float32(gridSize * cellSize)
+
+	// Calculate the starting position to center the grid
+	startX := -gridWidth / 2
+	startY := -gridHeight / 2
+
+	// Draw vertical lines
+	for i := 0; i <= gridSize; i++ {
+		x := startX + float32(i*cellSize)
+		rl.DrawLine3D(
+			rl.NewVector3(x, startY, 0),
+			rl.NewVector3(x, startY+gridHeight, 0),
+			rl.LightGray,
+		)
+	}
+
+	// Draw horizontal lines
+	for j := 0; j <= gridSize; j++ {
+		y := startY + float32(j*cellSize)
+		rl.DrawLine3D(
+			rl.NewVector3(startX, y, 0),
+			rl.NewVector3(startX+gridWidth, y, 0),
+			rl.LightGray,
+		)
+	}
+}
 
 type GameScene struct {
 	changeScene func(string)
@@ -8,6 +53,13 @@ type GameScene struct {
 
 func (s *GameScene) Init(changeScene func(string)) {
 	s.changeScene = changeScene
+	camera = rl.Camera3D{
+		Position:   rl.NewVector3(0, -300, 400),
+		Target:     rl.NewVector3(0, 0, 0),
+		Up:         rl.NewVector3(0, 1, 0),
+		Fovy:       45,
+		Projection: rl.CameraPerspective,
+	}
 }
 
 func (s *GameScene) Update() {
@@ -29,7 +81,60 @@ func (s *GameScene) Unload() {
 	// Unload game scene resources
 }
 
-func Input()          {}
-func DrawMap()        {}
+func Input() {
+	mousePos := rl.GetMousePosition()
+	isDragging := false
+	mousdelta := rl.GetMouseDelta()
+
+	// Handle camera zoom with mouse wheel
+	wheel := rl.GetMouseWheelMove()
+	if wheel != 0 {
+		camera.Position.Z -= wheel * 20
+		if camera.Position.Z < 50 {
+			camera.Position.Z = 50
+		}
+		if camera.Position.Z > 800 {
+			camera.Position.Z = 800
+		}
+	}
+
+	if rl.IsMouseButtonDown(rl.MouseLeftButton) {
+		isDragging = true
+		rl.DisableCursor()
+	}
+	if rl.IsMouseButtonReleased(rl.MouseButtonLeft) {
+		isDragging = false
+		rl.EnableCursor()
+	}
+
+	if isDragging {
+		camera.Position.X -= dragspeed * mousdelta.X
+		camera.Target.X -= dragspeed * mousdelta.X
+		camera.Target.Y += dragspeed * mousdelta.Y
+		camera.Position.Y += dragspeed * mousdelta.Y
+	}
+
+	if mousePos.X < float32(panBorder) {
+		camera.Position.X -= float32(panSpeed)
+		camera.Target.X -= float32(panSpeed)
+	} else if mousePos.X > float32(1600-panBorder) {
+		camera.Position.X += float32(panSpeed)
+		camera.Target.X += float32(panSpeed)
+	}
+	if mousePos.Y < float32(panBorder) {
+		camera.Position.Y += float32(panSpeed)
+		camera.Target.Y += float32(panSpeed)
+	} else if mousePos.Y > float32(900-panBorder) {
+		camera.Position.Y -= float32(panSpeed)
+		camera.Target.Y -= float32(panSpeed)
+	}
+
+}
+
+func DrawMap() {
+	rl.BeginMode3D(camera)
+	drawGrid()
+	rl.EndMode3D()
+}
 func DrawCharacters() {}
 func DrawUI()         {}
