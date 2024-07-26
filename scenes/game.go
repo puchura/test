9f17cc55d630e42cdc296f48ab8bfbc72517ebf9
Puchura <btc3171@gmail.com>
2024-07-26 13:09:20 +0400

    Rewrote some rendering:
    1. Rewrote map rendering
    2. Fixed: Left click return the correct position
    
    TODO: Rewrite character and map rendering

--- scenes/game.go
+++ scenes/game.go
@@ -8,19 +8,21 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const (
+	blocksize float32 = 5
+	dragspeed float32 = 0.15
+	panSpeed  float32 = 0.5
+	panBorder int     = 150
+)
+
 var (
-	gmap          core.GameMap
-	cr, grass     rl.Texture2D
-	cellWidth     int = 50
-	cellHeight    int = 50
-	camera        rl.Camera3D
-	panSpeed              = 5.0
-	panBorder             = 75
-	dragspeed     float32 = 0.25
-	selectedRect          = -1
-	isDragging    bool    = false
-	isKeyboardPan bool    = false
-	f             int     = 0
+	camera                    rl.Camera3D
+	mSizeX, mSizeZ, mSizeY    int = 10, 10, 10
+	gamemap                   core.GameMap
+	isDragging, isKeyboardPan bool = false, false
+	frame                     int  = 0
+	cr, grass                 rl.Texture2D
+	shader                    rl.Shader
 )
 
 type GameScene struct {
@@ -29,60 +31,187 @@ type GameScene struct {
 
 func (s *GameScene) Init(changeScene func(string)) {
 	s.changeScene = changeScene
-	camera = rl.Camera3D{
-		Position:   rl.NewVector3(0, -300, 400),
-		Target:     rl.NewVector3(0, 0, 0),
-		Up:         rl.NewVector3(0, 1, 0),
-		Fovy:       60,
-		Projection: rl.CameraPerspective,
-	}
-	x, y, t := maps.Test()
-	//fmt.Printf("%v, %v, %v", x, y, t)
-	gmap = core.NewMap(x, y, t)
-	gmap.GenerateMap()
+	camera = rl.NewCamera3D(
+		rl.Vector3{X: -50, Y: 30, Z: 0},
+		rl.Vector3{X: 0, Y: 0, Z: 0},
+		rl.Vector3{X: 0, Y: 1, Z: 0},
+		60,
+		rl.CameraPerspective,
+	)
+	gamemap = core.NewMap(maps.Test())
 	cr = rl.LoadTexture("res/Factions/Knights/Troops/Archer/Blue/Archer_Blue.png")
 	grass = rl.LoadTexture("res/Terrain/Ground/Tilemap_Flat.png")
-	//fmt.Printf("%v %v\n", cr.Height, cr.Width)
+	shader = rl.LoadShader("", "scenes/shader.fs")
 }
 
 func (s *GameScene) Update() {
 	if rl.IsKeyPressed(rl.KeyEscape) {
 		s.changeScene("menu")
 	}
-	Input()
+	input()
 }
 
 func (s *GameScene) Draw() {
 	//rl.DrawText("Game Scene (Press Esc to return to menu)", 100, 100, 20, rl.Black)
-
-	rl.DrawFPS(0, 0)
+	rl.BeginShaderMode(shader)
 	rl.BeginMode3D(camera)
+	rl.DrawGrid(10, float32(blocksize))
 	DrawMap()
 	DrawCharacters()
-	DrawUI()
 	rl.EndMode3D()
 
 }
 
 func (s *GameScene) Unload() {
-	// Unload scene resources
+	// Unload game scene resources
+}
+
+func getStartpoint() (float32, float32) {
+	return float32(-mSizeX) * blocksize / 2, float32(-mSizeZ) * blocksize / 2
+}
+
+func coordsToWorld(x, y, z float32) rl.Vector3 {
+	startX, startZ := getStartpoint()
+
+	worldX := startX + x*float32(blocksize) + float32(blocksize)/2
+	worldZ := startZ + z*float32(blocksize) + float32(blocksize)/2
+	worldY := float32(y)
+
+	return rl.Vector3{X: worldX, Y: worldY, Z: worldZ}
+}
+
+func input() {
+
+	//Zoom
+	wheel := rl.GetMouseWheelMove()
+	if wheel != 0 {
+		camera.Position.Y -= wheel * 4
+		if camera.Position.Y < 20 {
+			camera.Position.Y = 20
+		}
+		if camera.Position.Y > 50 {
+			camera.Position.Y = 50
+		}
+	}
+
+	// Select tile
+	if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+		ray := rl.GetMouseRay(rl.GetMousePosition(), camera)
+
+		closestHit := -1
+		closestDistance := float32(1000000) // Large initial value
+
+		for i := 0; i < len(gamemap.Tiles); i++ {
+			tile := gamemap.Tiles[i]
+			pos, _ := gamemap.GetTilePos(i)
+			tilePos := coordsToWorld(pos.Y, float32(tile.Height)*blocksize/4, pos.X)
+
+			// Create a bounding box for the tile
+			min := rl.Vector3{
+				X: tilePos.X - blocksize/2,
+				Y: tilePos.Y,
+				Z: tilePos.Z - blocksize/2,
+			}
+			max := rl.Vector3{
+				X: tilePos.X + blocksize/2,
+				Y: tilePos.Y + blocksize/4, // Assuming height is 1/4 of blocksize
+				Z: tilePos.Z + blocksize/2,
+			}
+
+			// Check for intersection
+			//distance := 0.0
+			hit := rl.GetRayCollisionBox(ray, rl.BoundingBox{Min: min, Max: max})
+
+			if hit.Hit && (closestHit == -1 || hit.Distance < closestDistance) {
+				closestHit = i
+				closestDistance = hit.Distance
+			}
+		}
+
+		if closestHit != -1 {
+			pos, _ := gamemap.GetTilePos(closestHit)
+			fmt.Printf("Clicked tile at grid position: X: %d, Z: %d, Height: %d\n",
+				int(pos.Y), int(pos.X), gamemap.Tiles[closestHit].Height)
+		}
+	}
+
+	// Mouse panning
+	if rl.IsMouseButtonDown(rl.MouseRightButton) {
+		mouseDelta := rl.GetMouseDelta()
+		isDragging = true
+
+		camera.Position.Z -= dragspeed * mouseDelta.X
+		camera.Target.Z -= dragspeed * mouseDelta.X
+		camera.Position.X += dragspeed * mouseDelta.Y
+		camera.Target.X += dragspeed * mouseDelta.Y
+	}
+	if rl.IsMouseButtonReleased(rl.MouseRightButton) {
+		isDragging = false
+	}
+
+	//Edge panning
+	if rl.IsCursorOnScreen() {
+		mousePos := rl.GetMousePosition()
+		if !isDragging && !isKeyboardPan {
+			if mousePos.X < float32(panBorder) {
+				camera.Position.Z -= float32(panSpeed)
+				camera.Target.Z -= float32(panSpeed)
+
+			} else if mousePos.X > float32(rl.GetScreenWidth()-panBorder) {
+				camera.Position.Z += float32(panSpeed)
+				camera.Target.Z += float32(panSpeed)
+			}
+			if mousePos.Y < float32(panBorder) {
+				camera.Position.X += float32(panSpeed)
+				camera.Target.X += float32(panSpeed)
+
+			} else if mousePos.Y > float32(rl.GetScreenHeight()-panBorder) {
+				camera.Position.X -= float32(panSpeed)
+				camera.Target.X -= float32(panSpeed)
+			}
+		}
+	}
+
+	// Keyboard panning
+	if rl.IsKeyDown(rl.KeyW) && !isDragging {
+		isKeyboardPan = true
+		camera.Position.X += float32(panSpeed)
+		camera.Target.X += float32(panSpeed)
+	}
+	if rl.IsKeyDown(rl.KeyS) && !isDragging {
+		isKeyboardPan = true
+		camera.Position.X -= float32(panSpeed)
+		camera.Target.X -= float32(panSpeed)
+	}
+	if rl.IsKeyDown(rl.KeyA) && !isDragging {
+		isKeyboardPan = true
+		camera.Position.Z -= float32(panSpeed)
+		camera.Target.Z -= float32(panSpeed)
+	}
+	if rl.IsKeyDown(rl.KeyD) && !isDragging {
+		isKeyboardPan = true
+		camera.Position.Z += float32(panSpeed)
+		camera.Target.Z += float32(panSpeed)
+	}
+
+	if rl.IsKeyReleased(rl.KeyW) || rl.IsKeyReleased(rl.KeyA) || rl.IsKeyReleased(rl.KeyS) || rl.IsKeyReleased(rl.KeyD) {
+		isKeyboardPan = false
+	}
+
 }
 
 func DrawMap() {
-	//drawGrid()
 	drawTerrain()
-
 }
 func DrawCharacters() {
-	RenderCharacters(gmap)
-	if f > 30 {
-		f = 0
+	RenderCharacters(gamemap)
+	if frame > 30 {
+		frame = 0
 		return
 	}
-	f++
+	frame++
 
 }
-func DrawUI() {}
 
 func terrainToColor(t core.Tile) rl.Color {
 	switch t.Terrain {
@@ -104,213 +233,51 @@ func terrainToColor(t core.Tile) rl.Color {
 func drawTerrain() {
 	rect := rl.Rectangle{X: 64, Y: 64, Width: 64, Height: 64}
 
-	for j := 0; j < gmap.SizeX*gmap.SizeY; j++ {
-		l, _ := gmap.GetTilePos(j)
-		m2w := MapToWorldCoords(int(l.X), int(l.Y), (gmap.Tiles[j].Height)*25/2)
-		if gmap.Tiles[j].Height > 0 {
+	for j := 0; j < gamemap.SizeX*gamemap.SizeY; j++ {
+		l, _ := gamemap.GetTilePos(j)
+		loc := coordsToWorld(l.Y, float32(gamemap.Tiles[j].Height)*blocksize/8, l.X)
+		if gamemap.Tiles[j].Height > 0 {
 			rl.DrawCube(
-				m2w,
-				50,
-				50,
-				float32(gmap.Tiles[j].Height)*25,
-				terrainToColor(gmap.Tiles[j]),
+				loc,
+				blocksize,
+				float32(gamemap.Tiles[j].Height)*blocksize/4,
+				blocksize,
+				terrainToColor(gamemap.Tiles[j]),
 			)
 		}
-		rl.DrawBillboardRec(camera, grass, rect, MapToWorldCoords(int(l.X), int(l.Y), 25*gmap.Tiles[j].Height), rl.Vector2{X: 50, Y: 50}, rl.White)
+		loc2 := coordsToWorld(l.Y, float32(gamemap.Tiles[j].Height)*blocksize/4, l.X)
+		rl.DrawBillboardPro(
+			camera,
+			grass,
+			rect,
+			loc2,
+			rl.Vector3{1, 0, 0},
+			rl.Vector2{blocksize, blocksize},
+			rl.Vector2{0, 0},
+			0,
+			rl.White)
+
 	}
 
-}
-
-func drawGrid() {
-	// Calculate the total grid dimensions
-	gridWidth := float32(gmap.SizeX * cellWidth)
-	gridHeight := float32(gmap.SizeY * cellHeight)
-
-	// Calculate the starting position to center the grid
-	startX := -gridWidth / 2
-	startY := -gridHeight / 2
-
-	for i := 0; i < gmap.SizeX; i++ {
-		for j := 0; j < gmap.SizeY; j++ {
-			x := startX + float32(i*cellWidth)
-			y := startY + float32(j*cellHeight)
-
-			// Draw filled rectangle for selected cell
-			if i*gmap.SizeX+j == selectedRect {
-				rl.DrawCube(
-					rl.NewVector3(x+float32(cellWidth)/2, y+float32(cellHeight)/2, 12.5),
-					float32(cellWidth),
-					float32(cellHeight),
-					25,
-					rl.ColorAlpha(rl.Blue, 0.5),
-				)
-			}
-
-			// Draw cell outline
-			rl.DrawLine3D(
-				rl.NewVector3(x, y, 0),
-				rl.NewVector3(x+float32(cellWidth), y, 0),
-				rl.LightGray,
-			)
-			rl.DrawLine3D(
-				rl.NewVector3(x+float32(cellWidth), y, 0),
-				rl.NewVector3(x+float32(cellWidth), y+float32(cellHeight), 0),
-				rl.LightGray,
-			)
-			rl.DrawLine3D(
-				rl.NewVector3(x+float32(cellWidth), y+float32(cellHeight), 0),
-				rl.NewVector3(x, y+float32(cellHeight), 0),
-				rl.LightGray,
-			)
-			rl.DrawLine3D(
-				rl.NewVector3(x, y+float32(cellHeight), 0),
-				rl.NewVector3(x, y, 0),
-				rl.LightGray,
-			)
-		}
-	}
 }
 
 func DrawBillboard(t rl.Texture2D, p rl.Vector3) {
 	vz := rl.GetCameraForward(&camera)
 	vx := rl.Vector3Normalize(rl.Vector3CrossProduct(vz, rl.Vector3{X: 0.0, Y: 1.0, Z: 0.0}))
 	vup := rl.Vector3Normalize(rl.Vector3CrossProduct(vx, vz))
-	src := rl.Rectangle{X: float32(192 * (f / 6)), Y: 0.0, Width: 192, Height: 192}
-	size := rl.Vector2{X: 50, Y: 50}
+	src := rl.Rectangle{X: float32(192 * (frame / 6)), Y: 0.0, Width: 192, Height: 192}
+	size := rl.Vector2{X: blocksize, Y: blocksize}
 	origin := rl.Vector2{X: 0, Y: 0}
 	rotation := 0.0
 	rl.DrawBillboardPro(camera, t, src, p, vup, size, origin, float32(rotation), rl.White)
 
 }
 
-func MapToWorldCoords(x, y, e int) rl.Vector3 {
-	gridWidth := float32(gmap.SizeX * cellWidth)
-	gridHeight := float32(gmap.SizeY * cellHeight)
-
-	startX := -gridWidth / 2
-	startY := -gridHeight / 2
-
-	worldX := startX + float32(x*cellWidth) + float32(cellWidth)/2
-	worldY := startY + float32(y*cellHeight) + float32(cellHeight)/2
-
-	worldZ := float32(e)
-
-	return rl.Vector3{X: worldX, Y: worldY, Z: worldZ}
-}
-
 func RenderCharacters(m core.GameMap) {
 	for i := len(m.Tiles) - 1; i >= 0; i-- {
 		if m.Tiles[i].Hitpoints == 0 {
 			pos, _ := m.GetTilePos(i)
-			DrawBillboard(cr, MapToWorldCoords(int(pos.X), int(pos.Y), (m.Tiles[i].Height*25)+10))
-		}
-		//fmt.Println(m.Tiles[i])
-	}
-}
-
-// DrawCubeTextureRec draws a textured cube using Raylib
-
-func Input() {
-	mousePos := rl.GetMousePosition()
-	mouseDelta := rl.GetMouseDelta()
-
-	// Handle camera zoom with mouse wheel
-	wheel := rl.GetMouseWheelMove()
-	if wheel != 0 {
-		camera.Position.Z -= wheel * 20
-		if camera.Position.Z < 75 {
-			camera.Position.Z = 75
-		}
-		if camera.Position.Z > 800 {
-			camera.Position.Z = 800
+			DrawBillboard(cr, coordsToWorld(pos.Y, float32(m.Tiles[i].Height)*((blocksize)/4)+1, pos.X))
 		}
 	}
-
-	// Handle left-click for rectangle selection
-	if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-		ray := rl.GetMouseRay(mousePos, camera)
-
-		// Calculate the intersection point
-		if ray.Direction.Z != 0 {
-			t := -ray.Position.Z / ray.Direction.Z
-			intersectionPoint := rl.Vector3Add(ray.Position, rl.Vector3Scale(ray.Direction, t))
-			// Calculate grid coordinates
-			gridWidth := float32(gmap.SizeX * cellWidth)
-			gridHeight := float32(gmap.SizeY * cellHeight)
-			startX := -gridWidth / 2
-			startY := -gridHeight / 2
-
-			gridX := int((intersectionPoint.X - startX) / float32(cellWidth))
-			gridY := int((intersectionPoint.Y - startY) / float32(cellHeight))
-
-			fmt.Printf("%v, %v, %v\n", gridX, gridY, intersectionPoint)
-			if gridX >= 0 && gridX < gmap.SizeX && gridY >= 0 && gridY < gmap.SizeY {
-				selectedRect = gridX*gmap.SizeX + gridY
-			}
-		}
-	}
-
-	// Handle right-click dragging for camera movement
-	if rl.IsMouseButtonDown(rl.MouseRightButton) {
-		isDragging = true
-
-		camera.Position.X -= dragspeed * mouseDelta.X
-		camera.Target.X -= dragspeed * mouseDelta.X
-		camera.Position.Y += dragspeed * mouseDelta.Y
-		camera.Target.Y += dragspeed * mouseDelta.Y
-	}
-	if rl.IsMouseButtonReleased(rl.MouseRightButton) {
-		isDragging = false
-	}
-
-	//Edge panning
-	if rl.IsCursorOnScreen() {
-		if !isDragging && !isKeyboardPan {
-			if mousePos.X < float32(panBorder) {
-				camera.Position.X -= float32(panSpeed)
-				camera.Target.X -= float32(panSpeed)
-
-				//TODO: replace 1600 with const representing window width
-			} else if mousePos.X > float32(1600-panBorder) {
-				camera.Position.X += float32(panSpeed)
-				camera.Target.X += float32(panSpeed)
-			}
-			if mousePos.Y < float32(panBorder) {
-				camera.Position.Y += float32(panSpeed)
-				camera.Target.Y += float32(panSpeed)
-
-				//TODO: replace 900 with const representing window height
-			} else if mousePos.Y > float32(900-panBorder) {
-				camera.Position.Y -= float32(panSpeed)
-				camera.Target.Y -= float32(panSpeed)
-			}
-		}
-	}
-
-	// Keyboard panning relative to camera angle
-	if rl.IsKeyDown(rl.KeyW) && !isDragging {
-		isKeyboardPan = true
-		camera.Position.Y += float32(panSpeed)
-		camera.Target.Y += float32(panSpeed)
-	}
-	if rl.IsKeyDown(rl.KeyS) && !isDragging {
-		isKeyboardPan = true
-		camera.Position.Y -= float32(panSpeed)
-		camera.Target.Y -= float32(panSpeed)
-	}
-	if rl.IsKeyDown(rl.KeyA) && !isDragging {
-		isKeyboardPan = true
-		camera.Position.X -= float32(panSpeed)
-		camera.Target.X -= float32(panSpeed)
-	}
-	if rl.IsKeyDown(rl.KeyD) && !isDragging {
-		isKeyboardPan = true
-		camera.Position.X += float32(panSpeed)
-		camera.Target.X += float32(panSpeed)
-	}
-
-	if rl.IsKeyReleased(rl.KeyW) || rl.IsKeyReleased(rl.KeyA) || rl.IsKeyReleased(rl.KeyS) || rl.IsKeyReleased(rl.KeyD) {
-		isKeyboardPan = false
-	}
-
 }
